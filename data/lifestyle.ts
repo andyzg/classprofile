63e699214d0722cae300e87a3abcf8ceac10d2a5
Andy Zhang <andzhng@gmail.com>
2018-06-09 10:01:14 -0400

    Add graphs, refactor, finalize lifestyle and end sections

---
+++ data/lifestyle.ts
@@ -0,0 +1,249 @@
+let EXERCISE = [{
+  "name": "Daily",
+  "value": 6,
+},
+{
+  "name": "Few times a week",
+  "value": 31,
+},
+{
+  "name": "Few times a month",
+  "value": 52,
+},
+{
+  "name": "Few times a year",
+  "value": 13,
+},
+{
+  "name": "Never",
+  "value": 11,
+}];
+
+let COOKING = [{
+  "name": "Daily",
+  "value": 17,
+},
+{
+  "name": "Few times a week",
+  "value": 29,
+},
+{
+  "name": "Few times a month",
+  "value": 41,
+},
+{
+  "name": "Few times a year",
+  "value": 11,
+},
+{
+  "name": "Never",
+  "value": 15,
+}];
+
+let SLEEPING = [{
+  "name": "10:00PM",
+  "value": 5,
+},
+{
+  "name": "11:00PM",
+  "value": 14,
+},
+{
+  "name": "12:00AM",
+  "value": 38,
+},
+{
+  "name": "1:00AM",
+  "value": 25,
+},
+{
+  "name": "2:00AM",
+  "value": 18,
+},
+{
+  "name": "3:00AM",
+  "value": 7,
+},
+{
+  "name": "4:00AM",
+  "value": 2,
+},
+{
+  "name": "5:00AM",
+  "value": 1,
+},
+{
+  "name": "6:00AM",
+  "value": 1,
+}];
+
+let LANGUAGES = [{
+  "name": "C",
+  "value": 2,
+}, {
+  "name": "C#",
+  "value": 1,
+}, {
+  "name": "C++",
+    "value": 11,
+}, {
+  "name": "Clojure",
+  "value": 1,
+}, {
+  "name": "Golang",
+  "value": 8,
+}, {
+  "name": "Java",
+  "value": 26,
+}, {
+  "name": "JavaScript",
+  "value": 18,
+}, {
+  "name": "Lua",
+  "value": 2,
+}, {
+  "name": "OCaml",
+  "value": 1,
+}, {
+  "name": "Objective-C",
+  "value": 1,
+}, {
+  "name": "Pascal",
+  "value": 1,
+}, {
+  "name": "Python",
+  "value": 33,
+}, {
+  "name": "Ruby",
+  "value": 3,
+}, {
+  "name": "Rust",
+  "value": 1,
+}, {
+  "name": "Scala",
+  "value": 9,
+}];
+
+let EDITOR = [{
+  "name": "Atom",
+  "value": 16,
+}, {
+  "name": "Eclipse",
+  "value": 2,
+}, {
+  "name": "IntelliJ",
+  "value": 11,
+}, {
+  "name": "JetBrains",
+  "value": 6,
+}, {
+  "name": "Notepad++",
+    "value": 3,
+}, {
+  "name": "Sublime",
+  "value": 49,
+}, {
+  "name": "Visual Studio Code",
+  "value": 7,
+}, {
+  "name": "Vim",
+  "value": 23,
+}];
+
+let SIDE = [{
+  "name": "0h/month",
+  "value": 27,
+}, {
+  "name": "1-10h/month",
+  "value": 63,
+}, {
+  "name": "10-20h/month",
+  "value": 9,
+}, {
+  "name": "20-30h/month",
+  "value": 5,
+}, {
+  "name": "30-40h/month",
+  "value": 3,
+}, {
+  "name": "40h+/month",
+  "value": 5,
+}]
+
+let DISCPLINES = [{
+  "name": "Back-end Web",
+  "value": 39,
+}, {
+  "name": "Front-end Web",
+  "value": 20,
+}, {
+  "name": "Machine Learning",
+  "value": 19,
+}, {
+  "name": "Artificial Intelligence",
+  "value": 16,
+}, {
+  "name": "Full-stack Web",
+  "value": 11,
+}, {
+  "name": "Security",
+  "value": 7,
+}, {
+  "name": "Big Data",
+  "value": 6,
+}, {
+  "name": "Design",
+  "value": 6,
+}, {
+  "name": "Product Management",
+  "value": 6,
+}, {
+  "name": "Graphics",
+  "value": 6,
+}, {
+  "name": "User Experience",
+  "value": 5,
+}, {
+  "name": "Data Science",
+  "value": 2,
+}, {
+  "name": "Desktop apps",
+  "value": 1,
+}, {
+  "name": "Distributed Systems",
+  "value": 2,
+}, {
+  "name": "Embedded Software",
+  "value": 4,
+}, {
+  "name": "Infrastructure",
+  "value": 3,
+}, {
+  "name": "Mobile Development",
+  "value": 3,
+}, {
+  "name": "Robotics",
+  "value": 2,
+}, {
+  "name": "System Architecture",
+  "value": 1,
+}, {
+  "name": "Theory & Implementation",
+  "value": 1,
+}, {
+  "name": "Virtualization",
+  "value": 1,
+}, {
+  "name": "Physics",
+  "value": 1,
+}]
+
+export {
+  EXERCISE,
+  COOKING,
+  SLEEPING,
+  LANGUAGES,
+  EDITOR,
+  SIDE,
+  DISCPLINES
+}
